27d53520571f880458cb3ae96f94a7373d7c37da
Rizvan Khan <rizvan3297@gmail.com>
2026-04-04 08:41:05 +0530

    register api create

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/name',function(){
+return ['name'=>'rizvan khan','age'=>'29'];
+});
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
