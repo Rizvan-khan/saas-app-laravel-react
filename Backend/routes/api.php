a2c8d12dfbeb76bc9b0676dd986a98ad6b1bff1b
Rizvan Khan <rizvan3297@gmail.com>
2026-04-05 18:18:05 +0530

    set route clear for api

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -5,17 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
+    Route::get('/v1/user', function (Request $request) {
         return $request->user();
     });
 
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/v1/logout', [AuthController::class, 'logout']);
+    Route::get('/v1/profile', [AuthController::class, 'profile']);
 });
 
-Route::get('/name', function () {
+Route::get('/v1/name', function () {
     return ['name' => 'rizvan khan', 'age' => '29', 'city' => 'New York'];
 });
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/v1/register', [AuthController::class, 'register']);
+Route::post('/v1/login', [AuthController::class, 'login']);
